44c662e88a40df4405bbe8e92e7be218d4c10a5a
Shaurya <shauryasingh1402@gmail.com>
2022-08-09 20:27:45 +0530

    Recursion

---
+++ Recursion/subsets.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+void subSets(string s, string curr , int idx){
+    if( idx == s.length())
+    {
+        cout << curr << " ";
+        return;
+    }subSets(s,curr,idx+1);
+    subSets(s,curr + s[idx],idx+1);
+}
+int main() {
+    
+    string s = "ABC";
+    subSets(s,"",0);
+    cout << endl;
+    return 0;
+}
