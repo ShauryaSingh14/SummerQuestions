44a7de5ae79f1e3e12a253c6d0c1f2f992407059
Shaurya <shauryasingh1402@gmail.com>
2022-08-22 08:24:38 +0530

    Love Babbar BST Q's

--- BST/deleteNodefromBST.cpp
+++ BST/deleteNodefromBST.cpp
@@ -13,12 +13,12 @@ class Node{
         this->right = NULL;
     }
 };
-int minVal(Node * root){
+Node * minVal(Node * root){
     if(!root)
     return NULL;
 
     if(!root->left)
-    return root->data;
+    return root;
 
     minVal(root->left);
 }
@@ -41,7 +41,7 @@ Node * deleteNode(Node * root,int val){
             return temp;
         }
         else{
-            int mini = minVal(root->right);
+            int mini = minVal(root->right)->data;
             root->data = mini;
             root->right= deleteNode(root->right,mini);
             return root;
@@ -52,16 +52,18 @@ Node * deleteNode(Node * root,int val){
         root->left = deleteNode(root->left, val);
     }
 }
-Node * insertNode(Node *& root,int val){
+Node * insertNode(Node * root,int val){
     if(!root){
-    Node * temp = new Node(val);
-    return temp;
+    root = new Node(val);
+    return root;
     }
+
     if(val > root->data)
     root->right = insertNode(root->right,val);
     else
     root->left = insertNode(root->left,val);
 
+    return root;
 }
 void levelOrderTrans(Node * root){
     cout << "Level Order Tranversal Of given tree :: " << endl;
@@ -91,21 +93,22 @@ void levelOrderTrans(Node * root){
         }
     }
 }
-Node * buildBST(){
+void buildBST(Node *&root){
     int data;
     cout << "Enter the root "<< endl;
     cin >> data;
-    Node * root = new Node(data);
     while(data != -1){
         root = insertNode(root,data);
         cout << "Enter the data : ";
         cin >> data;
     }
-    return root;
 }
 int main(){
-     Node * root = buildBST();
+
+     Node * root = NULL;
+    buildBST(root);
     levelOrderTrans(root);
+
     return 0;
 
 }
