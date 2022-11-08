ae19827b30176a7e20de1abd282fa2519bfdc882
Shaurya <shauryasingh1402@gmail.com>
2022-11-08 12:12:55 +0530

    DP-AV

--- DP/05-0/06-subsetSum.cpp
+++ DP/05-0/06-subsetSum.cpp
@@ -1,8 +1,7 @@
 //{ Driver Code Starts
 //Initial template for C++
 
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h> 
 using namespace std; 
 
 // } Driver Code Ends
@@ -10,58 +9,77 @@ using namespace std;
 
 class Solution{   
 public:
-bool knapSackRec(vector<int> arr,int n,int w){
-    if( n == 0){
-        if(w == 0)
-        return true;
-        
-        return false;
-    }
-    
-    if(arr[n-1] <= w ){
-    return knapSackRec(arr,n-1,w-arr[n-1]) || knapSackRec(arr,n-1,w);
-    }
-    else{
-    return knapSackRec(arr,n-1,w);
-    }
-}
-
-// bool knapSackMemo(vector<int> arr,int n,int w,
-//                  vector<vector<int>> &ans){
+// bool knapSackRec(vector<int> arr,int n,int w){
 //     if( n == 0){
 //         if(w == 0)
-//         return 1;
-
-//         return 0;
+//         return true;
+        
+//         return false;
 //     }
-//     if(ans[n-1][w] != -1)
+    
+//     if(arr[n-1] <= w ){
+//     return knapSackRec(arr,n-1,w-arr[n-1]) || knapSackRec(arr,n-1,w);
+//     }
+//     else{
+//     return knapSackRec(arr,n-1,w);
+//     }
+// }
+
+// bool knapSackMemo(vector<int> arr,int n,int w,
+//                  vector<vector<bool>> &ans){
+//     if( n == 0){
+//         if(w == 0)
+//         return true;
+
+//         return false;
+//     }
+
+//     if(ans[n-1][w] != 0)
 //     return ans[n][w];
     
 //     if(arr[n-1] <= w ){
-//     return ans[n][w] = knapSackMemo(arr,n-1,w-arr[n-1],ans) || knapSackMemo(arr,n-1,w,ans);
+//     ans[n][w] = knapSackMemo(arr,n-1,w-arr[n-1],ans) || knapSackMemo(arr,n-1,w,ans);
 //     }
 //     else{
-//     return ans[n][w] = knapSackMemo(arr,n-1,w,ans);
+//     ans[n][w] = knapSackMemo(arr,n-1,w,ans);
 //     }
     
+//     return ans[n][w];
 // }
-// int knapSackTopDown(vector<int> arr,int n, int w,
-//                     vector<vector<int>> & dp){
+
+bool knapSackTopDown(vector<int> arr,int n, int w,
+                    vector<vector<bool>> & dp){
     
-//     for(int i =0 ; i< n ;i++){
-//         for(int j = 0 ; j< w ;i++){
-//             dp[i][j]=
-            
-//         }
-//     }
+    for(int i =0 ; i< n+1 ;i++){
+        for(int j = 0 ; j< w+1 ;j++){
+
+            if(j == 0)
+            dp[i][j]=true;
+
+            if(i == 0)
+            dp[i][j]=false;
+        }
+    }
     
-// }
+    for(int i =1 ; i<n+1 ;i++){
+        for(int j =1 ; j<w+1 ;j++){
+            if(arr[i-1] <= j)
+            dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            else
+            dp[i][j] = dp[i-1][j];
+        }
+        
+    }
+    
+    return dp[n][w];
+}
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
        bool res = false;
-       int n = arr.size();
-    vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-    return knapSackRec(arr,arr.size(),sum);
+       int n = arr.size()-1;
+    vector<vector<bool>> dp(n+1,vector<bool>(sum+1));
+
+    return knapSackTopDown(arr,arr.size(),sum,dp);
         
        
        
