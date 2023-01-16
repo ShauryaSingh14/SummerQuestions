730c37927ef092296aba13c5101606e093a77fe9
Shaurya Singh <shauryasingh1402@gmail.com>
2023-01-16 11:43:33 +0530

    init

--- DP/AdityaVerma/10-minDiffSubset.cpp
+++ DP/AdityaVerma/10-minDiffSubset.cpp
@@ -6,6 +6,8 @@ using namespace std;
 class Solution{
 
   public:
+
+  // Recursive Code 
   int minDiffRec(int arr[],int n,int s1,int s2){
       if(n == 0){
           return abs(s1- s2);
@@ -15,7 +17,7 @@ class Solution{
      
   }
   
-  
+  // Memo Code 
   int minDiffMemo(int arr[],int n,int s1,int s2,
         vector<vector<int>> &dp){
      if(n == 0){
@@ -25,29 +27,19 @@ class Solution{
       if(dp[n][s1] != -1 )
       return dp[n][s1];
       
-      return dp[n][s1] = min(minDiffRec(arr,n-1,s1,s2),
-      minDiffRec(arr,n-1,s1+arr[n-1],s2-arr[n-1]));
+      return dp[n][s1] = min(minDiffMemo(arr,n-1,s1,s2,dp),
+      minDiffMemo(arr,n-1,s1+arr[n-1],s2-arr[n-1],dp));
   }
-  
-  
-  int minDiffTabulor(int arr[],int n,int s1,int s2,
-        vector<vector<int>> &dp){
-    
-    for(int i =0 ; i<= n ;i++){
-        for(int j =0 ; j<= s2 ;j++){
-            if(i == 0)
-            dp[i][j] = abs(s1 - s2);
-        }
-    }
-    
-    for(int i = 1; i<=n;i++){
-        for(int j = 1; j<=s2 ;j++){
-            dp[i][j] = min(dp[i-1][s1+arr[i-1]],dp[i-1][s1]);
-        }
-    }
-    return dp[n][s1];
-    
-        }
+	int minDifference(int arr[], int n)  { 
+	    int s1 =0;
+	    int s2 =0;
+	    
+	    for(int i =0 ; i< n;i++)
+	    s2 += arr[i];
+	    
+	    vector<vector<int>> dp(n+1,vector<int>(s2+1,-1));
+	    return minDiffMemo(arr,n,s1,s2,dp);
+	} 
         
  // Aditya Verma Approch
   int tabulorApproch( int arr[], int n,int sum){
@@ -97,24 +89,6 @@ class Solution{
 	    }
 	    
 	    return tabulorApproch(arr,n,sum);
-	} 
-
-
-	int minDifference(int arr[], int n)  { 
-	    // Your code goes here
-	    int s1 =0;
-	    int s2 = 0;
-
-	   for(int i =0 ; i< n ;i++){
-	       s2 += arr[i];
-	   }
-	   
-	   vector<vector<int>> dp(n+1,vector<int>(s2+1,0));
-	   //return minDiffRec(arr,n,s1,s2);   
-	   //return minDiffMemo(arr,n,s1,s2,dp);  
-	   return minDiffTabulor(arr,n,s1,s2,dp);
-	   
-	   
 	} 
 };
 
